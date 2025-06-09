4754b9e15bc3e12e498847907f3acce49b13298c
VIMALCHINTU <158822830+VIMALCHINTU@users.noreply.github.com>
2025-06-09 13:01:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const isPowerOfTwo = (number) => {
   //your code here
+	if (number < 1) return false;
+
+  while (number > 1) {
+    if (number % 2 !== 0) {
+      return false;
+    }
+    number = number / 2;
+  }
+
+  return true;
 };
+
+
 
 const num = parseInt(prompt("Enter a number"));
 alert(isPowerOfTwo(num));
